51e37629867911848fb1b3305e6beca21168ad19
BalajiKgithub <balajikamarajmallika@gmail.com>
2023-01-31 18:42:04 +0530

    My task

---
+++ src/org/hcl/GitClassSample.java
@@ -0,0 +1,20 @@
+package org.hcl;
+
+public class GitClassSample {
+	
+	private void empId() {
+		System.out.println("empId is 12345");
+	}
+	
+	private void empName() {
+		System.out.println("empName is BALA");
+	}
+
+	public static void main(String[] args) {
+
+		GitClassSample g = new GitClassSample();
+		g.empId();
+		g.empName();
+	}
+
+}
